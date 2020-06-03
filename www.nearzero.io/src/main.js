635cb39c605baefcd81a046a6b929afd5fcb4f41
Mark Smith <msmith4@gmail.com>
2020-06-03 02:16:13 +0000

    Replacing default vue-cli site with vue-material-kit-pro

--- www.nearzero.io/src/main.js
+++ www.nearzero.io/src/main.js
@@ -1,12 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// =========================================================
+// * Vue Material Kit PRO - v1.2.2
+// =========================================================
+//
+// * Product Page: https://www.creative-tim.com/product/vue-material-kit-pro
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+//
+// * Coded by Creative Tim
+//
+// =========================================================
+//
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from "./router";
+
+import MaterialKit from "./plugins/material-kit";
+
+Vue.config.productionTip = false;
+
+Vue.use(MaterialKit);
 
 new Vue({
   router,
-  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
